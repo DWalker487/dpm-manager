ee4bef6060f4eff3b82df4a9cad5d3cc55ca2ef8
Marian Heil <48517413+marianheil@users.noreply.github.com>
2019-08-22 14:39:11 +0100

    document/list all protocols
    There might be more protocols that I don't know yet

--- config.py
+++ config.py
@@ -3,6 +3,13 @@ import os
 default_user = os.environ["USER"]
 file_count_reprint_no = 15
 DPM = "://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/{0}/"
+## possible protocols:
+## recommended:
+# gsiftp, srm (slow, but reliable), dav, davs, xroot
+## less reliable protocols:
+# root, https, s3, lfc, guid, rfio, dcap
+## not supported by gfal:
+# sftp, mock
 protocol_default = "gsiftp"
 protocol_list = "dav"
 protocol_delete = "xroot"
