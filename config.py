e428da68a569e8080376d6248bc0a8c39e6c17b5
Marian Heil <48517413+marianheil@users.noreply.github.com>
2020-04-08 14:07:57 +0200

    mark srm as deprecated

--- config.py
+++ config.py
@@ -5,18 +5,20 @@ file_count_reprint_no = 15
 DPM = "://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/{0}/"
 ## possible protocols:
 ## recommended:
-# gsiftp, srm (slow, but reliable), dav, davs, xroot
+# gsiftp, dav, davs, xroot
 ## less reliable protocols:
 # root, https, s3, lfc, guid, rfio, dcap
 ## not supported by gfal:
 # sftp, mock
+## deprecated
+# srm
 protocol_default = "gsiftp"
 protocol_list = "dav"
 protocol_delete = "xroot"
 protocol_download = "xroot"
 protocol_upload = "xroot"
 protocol_move = "xroot"
-protocol_mkdir = "srm"
+protocol_mkdir = "gsiftp"
 dir_colour = 33
 exe_colour = 34
 use_fnmatch = False
